d8c4f466fdc9eb7df469854bd61fbf9281a2af35
DOGEC <dogecash@protonmail.com>
2018-12-09 20:40:22 +0300

    add

--- src/qt/outputspage.cpp
+++ src/qt/outputspage.cpp
@@ -1,90 +1,82 @@
 // Copyright (c) 2018 The Phore developers
- // Copyright (c) 2018 The Curium developers
- // Distributed under the MIT/X11 software license, see the accompanying
- // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2018 The Curium developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
- #if defined(HAVE_CONFIG_H)
- #include "config/dogecash-config.h"
- #endif
+#if defined(HAVE_CONFIG_H)
+#include "config/dogecash-config.h"
+#endif
 
- #include "outputspage.h"
- #include "ui_outputspage.h"
+#include "outputspage.h"
+#include "ui_outputspage.h"
 
- #include "activemasternode.h"
- #include "bitcoingui.h"
- #include "csvmodelwriter.h"
- #include "editaddressdialog.h"
- #include "guiutil.h"
- #include "masternode-budget.h"
- #include "masternode-payments.h"
- #include "masternodeconfig.h"
- #include "masternodeman.h"
-#include "json/json_spirit_value.h"
- #include "univalue/univalue.h"
- #include <QIcon>
- #include <QMenu>
- #include <QString>
- #include <QMessageBox>
- #include <QSortFilterProxyModel>
- #include <boost/tokenizer.hpp>
- #include <fstream>
-#include <stdint.h>
-#include <string.h>
+#include "activemasternode.h"
+#include "bitcoingui.h"
+#include "csvmodelwriter.h"
+#include "editaddressdialog.h"
+#include "guiutil.h"
+#include "masternode-budget.h"
+#include "masternode-payments.h"
+#include "masternodeconfig.h"
+#include "masternodeman.h"
 
-#include <string>
-#include <vector>
-#include <map>
-#include <cassert>
+#include <univalue.h>
+#include <QIcon>
+#include <QMenu>
+#include <QString>
+#include <QMessageBox>
+#include <QSortFilterProxyModel>
+#include <boost/tokenizer.hpp>
+#include <fstream>
 
-#include <sstream>        // .get_int64()
- OutPutsPage::OutPutsPage(QWidget* parent) : QDialog(parent),
-                                             ui(new Ui::OutPutsPage)
- {
-     ui->setupUi(this);
+OutPutsPage::OutPutsPage(QWidget* parent) : QDialog(parent),
+                                            ui(new Ui::OutPutsPage)
+{
+    ui->setupUi(this);
 
- 	key = OutPutsPage::getmasternodeoutputs();
- 	ui->outPutsWidget->append(key);
+	key = OutPutsPage::getmasternodeoutputs();
+	ui->outPutsWidget->append(key);
 
-     // Build context menu
-     contextMenu = new QMenu();
-     contextMenu->addSeparator();
+    // Build context menu
+    contextMenu = new QMenu();
+    contextMenu->addSeparator();
 
 
-     connect(ui->closeButton, SIGNAL(clicked()), this, SLOT(accept()));
- }
+    connect(ui->closeButton, SIGNAL(clicked()), this, SLOT(accept()));
+}
 
 
 
- OutPutsPage::~OutPutsPage()
- {
-     delete ui;
- }
+OutPutsPage::~OutPutsPage()
+{
+    delete ui;
+}
 
 
 
 
- QString OutPutsPage::getmasternodeoutputs ()
- {
-     // Find possible candidates
-     vector<COutput> possibleCoins = activeMasternode.SelectCoinsMasternode();
+QString OutPutsPage::getmasternodeoutputs ()
+{
+    // Find possible candidates
+    vector<COutput> possibleCoins = activeMasternode.SelectCoinsMasternode();
 
-     UniValue result(UniValue::VARR);
-     BOOST_FOREACH (COutput& out, possibleCoins) {
-         UniValue obj(UniValue::VOBJ);
-         obj.push_back(Pair("txhash", out.tx->GetHash().ToString()));
-         obj.push_back(Pair("outputidx", out.i));
-         result.push_back(obj);
-     }
- 	
-         std::string strPrint;
+    UniValue result(UniValue::VARR);
+    BOOST_FOREACH (COutput& out, possibleCoins) {
+        UniValue obj(UniValue::VOBJ);
+        obj.push_back(Pair("txhash", out.tx->GetHash().ToString()));
+        obj.push_back(Pair("outputidx", out.i));
+        result.push_back(obj);
+    }
+	
+        std::string strPrint;
 
-         // Format result reply
-         if (result.isNull())
-             strPrint = "";
-         else if (result.isStr())
-             strPrint = result.get_str();
-         else
-             strPrint = result.write(2);
+        // Format result reply
+        if (result.isNull())
+            strPrint = "";
+        else if (result.isStr())
+            strPrint = result.get_str();
+        else
+            strPrint = result.write(2);
 
-     return QString::fromStdString(strPrint);
- }
+    return QString::fromStdString(strPrint);
+}
