013adc5f11c31637c560883c761552d585df0b71
DOGEC <dogecash@protonmail.com>
2018-12-09 19:57:51 +0300

    unival

--- src/qt/outputspage.cpp
+++ src/qt/outputspage.cpp
@@ -20,7 +20,7 @@
  #include "masternodeconfig.h"
  #include "masternodeman.h"
 #include "json/json_spirit_value.h"
- #include "univalue/univalue.h"
+ #include <univalue/univalue.h">
  #include <QIcon>
  #include <QMenu>
  #include <QString>
