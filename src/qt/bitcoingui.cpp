c473d6ea7c82dcf85b6e50c68f1173f5d6901b4a
YunLai <yunlai0086@gmail.com>
2019-01-27 22:35:13 +0800

    as

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -117,9 +117,9 @@ BitcoinGUI::BitcoinGUI(const NetworkStyle* networkStyle, QWidget* parent) : QMai
 //    this->setStyleSheet(GUIUtil::loadStyleSheet());
 //    this->setStyleSheet("background-color: blue");
     this->setStyleSheet("background-color: qradialgradient(spread:pad, cx:0.29, cy:0.23, radius:1, fx:0.29, fy:0.23,  stop:0 #44505e, stop:1#262d35);");
-    GUIUtil::restoreWindowGeometry("nWindow", QSize(990, 848), this);
-    this->setMinimumHeight(990);
-    this->setMinimumWidth(848);
+    GUIUtil::restoreWindowGeometry("nWindow", QSize(1178, 760), this);
+    this->setMinimumHeight(1178);
+    this->setMinimumWidth(760);
     
     
 
