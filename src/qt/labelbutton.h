bf8f279da4b9787205a9bf61c6f25ca2adb6b8c4
YunLai <yunlai0086@gmail.com>
2019-01-28 11:31:50 +0800

    test

--- src/qt/labelbutton.h
+++ src/qt/labelbutton.h
@@ -9,7 +9,7 @@ class LabelButton : public QLabel
     Q_OBJECT
     
 public:
-    explicit LabelButton(QWidget* parent);
+    explicit LabelButton(QWidget* parent = 0);
     LabelButton(QWidget *parent, QRect geo, QString imageString);
     LabelButton(QWidget *parent, int i);
 
