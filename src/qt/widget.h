e18c5cff5f4afa114c2e2255c83b42842b04cc1a
YunLai <yunlai0086@gmail.com>
2019-01-28 18:34:28 +0800

    test

--- src/qt/widget.h
+++ src/qt/widget.h
@@ -31,7 +31,7 @@ class Widget : public QWidget
 
 public:
     explicit Widget(QWidget *parent = nullptr);
-    ~Widget();
+    virtual ~Widget();
 
     QPixmap *pixmap_home, *pixmap_send, *pixmap_receive, *pixmap_mns, *pixmap_history;
     QIcon *homeIcon, *sendIcon, *receiveIcon, *mnsIcon, *historyIcon ;
