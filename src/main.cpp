3b534812e8936aa0c535527dac9125cd77ddebad
Akshay <akshaycm@hotmail.com>
2019-02-02 22:57:19 +0300

    Revert "fix minprotcol"
    This reverts commit 246afe557b21ca151879901f6141d37623b94801.

--- src/main.cpp
+++ src/main.cpp
@@ -105,14 +105,7 @@ unsigned int GetStakeMinAge(int nHeight)
 
 int GetMinPeerProtoVersion(int nHeight)
 {
-    if (IsSporkActive(SPORK_14_NEW_PROTOCOL_ENFORCEMENT))
-            return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT;
-
-    // SPORK_15 was used for 70912 (v3.0.5+), commented out now.
-    //if (IsSporkActive(SPORK_15_NEW_PROTOCOL_ENFORCEMENT_2))
-    //        return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT;
-    else
-    return MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT;
+		return PROTOCOL_VERSION; //2.2.0
 
 }
 
