d431bc5999cab841f6859fb067fbb5f0bf70dc07
purlejina <purlejina6131@gmail.com>
2019-03-11 07:38:57 -0700

    replace svg file to large scale png file

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -235,9 +235,9 @@ BitcoinGUI::BitcoinGUI(const NetworkStyle* networkStyle, QWidget* parent) : QMai
     // as they make the text unreadable (workaround for issue #1071)
     // See https://qt-project.org/doc/qt-4.8/gallery.html
     QString curStyle = QApplication::style()->metaObject()->className();
-    if (curStyle == "QWindowsStyle" || curStyle == "QWindowsXPStyle") {
-        progressBar->setStyleSheet("QProgressBar { background-color: #F8F8F8; border: 1px solid grey; border-radius: 7px; padding: 1px; text-align: center; } QProgressBar::chunk { background: QLinearGradient(x1: 0, y1: 0, x2: 1, y2: 0, stop: 0 #00CCFF, stop: 1 #33CCFF); border-radius: 7px; margin: 0px; }");
-    }
+//    if (curStyle == "QWindowsStyle" || curStyle == "QWindowsXPStyle") {
+//        progressBar->setStyleSheet("QProgressBar { background-color: #F8F8F8; border: 1px solid grey; border-radius: 7px; padding: 1px; text-align: center; } QProgressBar::chunk { background: QLinearGradient(x1: 0, y1: 0, x2: 1, y2: 0, stop: 0 #00CCFF, stop: 1 #33CCFF); border-radius: 7px; margin: 0px; }");
+//    }
 
     QWidget * emptyWidget = new QWidget();
     emptyWidget->setObjectName("EmptyWidget");
