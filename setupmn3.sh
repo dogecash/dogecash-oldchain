9ee33c1bdd41db47cdbab978ab832b9439ea9f15
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-03-14 20:10:36 -0500

    Update setupmn3.sh

--- setupmn3.sh
+++ setupmn3.sh
@@ -115,8 +115,9 @@ EOF
 
   systemctl daemon-reload
   sleep 10
-  systemctl start $COIN_NAME.service
   systemctl enable $COIN_NAME.service
+  sleep 5
+  systemctl start $COIN_NAME.service
 
   if [[ -z "$(ps axo cmd:100 | egrep $COIN_DAEMON)" ]]; then
     echo -e "${RED}$COIN_NAME is not running${NC}, please investigate. You should start by running the following commands as root:"
