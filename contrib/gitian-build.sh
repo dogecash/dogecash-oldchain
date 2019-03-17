11e583c0882f10c2de7ccb0db8967ad05f659f56
Akshay CM <akshaycm@hotmail.com>
2019-03-17 17:21:15 +0300

    Update gitian-build.sh

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -237,8 +237,8 @@ echo ${COMMIT}
 if [[ $setup = true ]]
 then
     sudo apt-get install ruby apache2 git apt-cacher-ng python-vm-builder qemu-kvm qemu-utils
-    git clone https://github.com/dogecash/dogecash/gitian.sigs.git
-    git clone https://github.com/dogecash/dogecash/dogecash-detached-sigs.git
+    git clone https://github.com/dogecash/gitian.sigs.git
+    git clone https://github.com/dogecash/dogecash-detached-sigs.git
     git clone https://github.com/devrandom/gitian-builder.git
     pushd ./gitian-builder
     if [[ -n "$USE_LXC" ]]
