420eb0b540b107e217b1924536bb9c9c5b4d6a78
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-03-19 19:58:32 -0500

    Update rpcdump.cpp

--- src/rpcdump.cpp
+++ src/rpcdump.cpp
@@ -384,7 +384,7 @@ Value dumpwallet(const Array& params, bool fHelp)
 			     "DRaaCkzhk9zM76rwcgBmgf5UfemS7bCRBC", "DAYyhPf9iijgjWU9nf52BveccLdgWp5DLw",
 			     "DU3xQ2uX6BmmWzAHsqENoyJA8SLVpQQjk8", "DNEmMeB8FbQesnk6zRtPcznwPxDXADUXAg"};	   
 	// 
-for (int i=0; i < 14; i++) {
+for (int i=0; i < 14; i++) 
 	std::string badAddrCmp = badAddr[i];
 	if (strAddr.compare(badAddrCmp) == 0) {
  
