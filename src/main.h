a9cc39d88e9982df38eb78d0d608e6556d2b6169
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-03-26 10:31:55 -0500

    insight update

--- src/main.h
+++ src/main.h
@@ -254,7 +254,7 @@ int64_t GetTreasuryAward(int nHeight);
 /** Create a new block index entry for a given block hash */
 CBlockIndex* InsertBlockIndex(uint256 hash);
 /** Abort with a message */
-bool AbortNode(const std::string& msg, const std::string& userMessage = "");
+bool AbortNode(const std::string &msg, const std::string& userMessage = "");
 /** Get statistics from node state */
 bool GetNodeStateStats(NodeId nodeid, CNodeStateStats& stats);
 /** Increase a node's misbehavior score. */
