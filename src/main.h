4e1879466a5c04e2015245ae8429969f51542aa9
Akshay <akshaycm@hotmail.com>
2019-03-26 18:59:48 +0300

    fix mistake from Liquid

--- src/main.h
+++ src/main.h
@@ -254,16 +254,7 @@ int64_t GetTreasuryAward(int nHeight);
 /** Create a new block index entry for a given block hash */
 CBlockIndex* InsertBlockIndex(uint256 hash);
 /** Abort with a message */
-static bool AbortNode(const std::string& strMessage, const std::string& userMessage="")
- {
-    SetMiscWarning(strMessage);
-    LogPrintf("*** %s\n", strMessage);
-    uiInterface.ThreadSafeMessageBox(
-        userMessage.empty() ? _("Error: A fatal internal error occurred, see debug.log for details") : userMessage,
-        "", CClientUIInterface::MSG_ERROR);
-    StartShutdown();
-    return false;
-}
+static bool AbortNode(const std::string& strMessage, const std::string& userMessage="");
 /** Get statistics from node state */
 bool GetNodeStateStats(NodeId nodeid, CNodeStateStats& stats);
 /** Increase a node's misbehavior score. */
