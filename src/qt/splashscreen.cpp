b238bedf991a46e3f7e7747602202182d3122464
liquid369@gmail.com <Liquid369@gmail.com>
2019-04-10 10:37:21 -0500

    Text color fix
    Signed-off-by: liquid369@gmail.com <Liquid369@gmail.com>

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -46,7 +46,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle* networkStyle) 
     // load the bitmap for writing some text over it
     pixmap = networkStyle->getSplashImage();
     QPainter pixPaint(&pixmap);
-    pixPaint.setPen(QColor(0, 0, 0));
+    pixPaint.setPen(QColor(100, 100, 100));
 
     // check font size and drawing with
     pixPaint.setFont(QFont(font, 28 * fontFactor));
@@ -113,7 +113,7 @@ static void InitMessage(SplashScreen* splash, const std::string& message)
         Qt::QueuedConnection,
         Q_ARG(QString, QString::fromStdString(message)),
         Q_ARG(int, Qt::AlignBottom | Qt::AlignHCenter),
-        Q_ARG(QColor, QColor(1, 0, 0)));
+        Q_ARG(QColor, QColor(100, 100, 100)));
 }
 
 static void ShowProgress(SplashScreen* splash, const std::string& title, int nProgress)
