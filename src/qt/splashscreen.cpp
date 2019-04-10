fd01788e3c2a6c13d32cd57b0859461e08883d75
liquid369@gmail.com <Liquid369@gmail.com>
2019-04-10 14:32:15 -0500

    splash text fix
    Signed-off-by: liquid369@gmail.com <Liquid369@gmail.com>

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -26,7 +26,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle* networkStyle) 
 {
     // set reference point, paddings
     int paddingLeft = 21;
-    int paddingTop = 570;
+    int paddingTop = 650;
     int titleVersionVSpace = 17;
     int titleCopyrightVSpace = 32;
 
