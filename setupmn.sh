0aa23f300859abb80d02a9ee5c7e72ceea14bb56
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2019-04-22 11:51:13 -0500

    Update setupmn.sh

--- setupmn.sh
+++ setupmn.sh
@@ -386,6 +386,7 @@ function important_information() {
  echo -e "VPS_IP:PORT ${RED}$NODEIP:$COIN_PORT${NC}"
  echo -e "MASTERNODE PRIVATEKEY is: ${RED}$COINKEY${NC}"
  echo -e "Please check ${RED}$COIN_NAME${NC} is running with the following command: ${RED}systemctl status $COIN_NAME.service${NC}"
+ systemctl status $COIN_NAME.service
  echo -e "================================================================================================================================"
 }
 
